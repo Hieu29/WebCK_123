fe63fe3d8f6879c23a8f20d88331e2ee65a0371d
HieuTK <kimhieu1314@gmail.com>
2020-07-19 21:00:27 +0700

    TKH

---
+++ Doanweb/View/User/Aboutus.php
@@ -0,0 +1,46 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<section class="our-webcoderskull padding-lg">
+  <div style="background-image: url('Images/168419.jpg')" class="container">
+    <div class="row heading heading-icon">
+        <h2 style="font-family: tahoma; font-weight: bold; color: #fff">My Team</h2>
+    </div>
+    <ul style="margin-left: 200px;" class="row">
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3><a href="https://www.facebook.com/kimhieu1314">Trần Kim Hiếu</a></h3>
+            <p style="font-weight: bold;color: #ff6600; font-size: 16px;">43.01.104.051</p>
+            <ul class="follow-us clearfix">
+              <li><a href="https://www.facebook.com/kimhieu1314"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://instagram.com/?lang=vi"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="http://www.webcoderskull.com/img/team3.png" class="img-responsive" alt=""></figure>
+            <h3><a href="https://www.facebook.com/pham.minhluan.587">Phạm Minh Luân</a></h3>
+            <p style="font-weight: bold;color: #ff6600; font-size: 16px">43.01.104.102</p>
+            <ul class="follow-us clearfix">
+              <li><a href="https://www.facebook.com/pham.minhluan.587"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://instagram.com/?lang=vi"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="http://www.webcoderskull.com/img/team2.png" class="img-responsive" alt=""></figure>
+            <h3><a href="https://www.facebook.com/profile.php?id=100008530577426">Phan Thanh Hữu</a></h3>
+            <p style="font-weight: bold;color: #ff6600; font-size: 16px">43.01.104.064</p>
+            <ul class="follow-us clearfix">
+              <li><a href="https://www.facebook.com/profile.php?id=100008530577426"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://instagram.com/?lang=vi"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+       </li>
+    </ul>
+  </div>
+</section>
